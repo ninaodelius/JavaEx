58ed44a762d2803a086b6f6e698b4c83fa971f0f
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2023-02-07 19:11:43 +0100

    appsecurityconfig + appviewconfig to link frontend

--- src/main/java/com/example/javaex/config/AppSecurityConfig.java
+++ src/main/java/com/example/javaex/config/AppSecurityConfig.java
@@ -1,4 +1,45 @@
 package com.example.javaex.config;
 
+import com.example.javaex.user.UserModelDetailsService;
+import com.example.javaex.user.workout.WorkoutModelDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Profile("!https")
 public class AppSecurityConfig {
+
+    private final UserModelDetailsService userModelDetailsService;
+    private final WorkoutModelDetailsService workoutModelDetailsService;
+    private final AppPasswordConfig appPasswordConfig;
+
+    public AppSecurityConfig(UserModelDetailsService userModelDetailsService, WorkoutModelDetailsService workoutModelDetailsService, AppPasswordConfig appPasswordConfig) {
+        this.userModelDetailsService = userModelDetailsService;
+        this.workoutModelDetailsService = workoutModelDetailsService;
+        this.appPasswordConfig = appPasswordConfig;
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterchain(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers(
+                        HttpMethod.GET,
+                        "/index*", "/static/**", "/*.js", "/*.json", "/*.ico")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/index.html")
+                .loginProcessingUrl("/perform_login")
+                .defaultSuccessUrl("/homepage.html",true)
+                .failureUrl("/index.html?error=true");
+        return http.build();
+    }
 }
--- src/main/java/com/example/javaex/config/AppViewConfig.java
+++ src/main/java/com/example/javaex/config/AppViewConfig.java
@@ -13,5 +13,15 @@ public class AppViewConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
 
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("/JavaScriptEx/frontend/build/static/");
+        registry.addResourceHandler("/*.js")
+                .addResourceLocations("e/JavaScriptEx/frontend/build/");
+        registry.addResourceHandler("/*.json")
+                .addResourceLocations("/JavaScriptEx/frontend/build/");
+        registry.addResourceHandler("/*.ico")
+                .addResourceLocations("/JavaScriptEx/frontend/build/");
+        registry.addResourceHandler("/index.html")
+                .addResourceLocations("/JavaScriptEx/frontend/build/index.html");
     }
 }
