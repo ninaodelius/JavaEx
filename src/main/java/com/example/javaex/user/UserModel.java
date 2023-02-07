67f72bbf1e27ea131cfad3e82395ebf783dcd13e
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2023-02-07 14:54:06 +0100

    UserModel entity

--- src/main/java/com/example/javaex/user/UserModel.java
+++ src/main/java/com/example/javaex/user/UserModel.java
@@ -1,4 +1,66 @@
 package com.example.javaex.user;
 
-public class UserModel {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "users")
+public class UserModel implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 2, max = 30)
+    private String name;
+
+    @NotEmpty
+    @Size(min = 4, max = 50)
+    @Email
+    private String email;
+
+    @NotEmpty
+    @Size(min = 6, max = 200)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<SimpleGrantedAuthority> authorities;
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
