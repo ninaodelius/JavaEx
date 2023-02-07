77c81a059439f89ea8489a8deab67382c5e165a4
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2023-02-07 14:55:23 +0100

    Repository

--- src/main/java/com/example/javaex/user/UserModelRepository.java
+++ src/main/java/com/example/javaex/user/UserModelRepository.java
@@ -1,4 +1,8 @@
 package com.example.javaex.user;
 
-public interface UserModelRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserModelRepository extends JpaRepository<UserModel, Long> {
 }
